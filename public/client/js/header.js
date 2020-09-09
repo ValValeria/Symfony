c3f630a40e02117ad3fcf9a7804d384abc6d7ea7
ValValeria <led2017l@gmail.com>
2020-09-09 18:21:24 +0300

    init commit

---
+++ public/client/js/header.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded',()=>{
+    const path = window.location.pathname;
+    
+    const links = Array.from(document.querySelectorAll('nav .nav-link'))
+
+    links.forEach(el=>{
+        const elPath = el.getAttribute('href');
+        
+        if(elPath==path){
+            el.classList.add('active')
+        } else {
+            el.classList.remove('active')
+        }
+    })
+})
---
+++ src/Controller/FileLoading.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+
+use Symfony\Component\Routing\Annotation\Route;
+
+use Symfony\Component\HttpFoundation\Request;
+
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
+
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+
+
+class FileLoading {
+    
+    protected $parameterBag;
+    protected const mimeTypes = ["css"=>"text/css","js"=>"text/javascript"];
+
+    public function __construct(ParameterBagInterface $parameterBag)
+    {
+        $this->parameterBag = $parameterBag;
+    }
+    
+    
+    /**
+     * Handles file request, located in public\client folder
+     */
+    public function fileHandling(Request $request,string $dirname,string $filename):Response
+    {
+        $path = $this->parameterBag->get('kernel.project_dir')."\public\client\\$dirname\\$filename";
+        $response = new Response();
+        $extension = pathinfo($path,PATHINFO_EXTENSION);
+
+        if (file_exists($path) && in_array($extension,array_keys(self::mimeTypes))) {
+            $content = file_get_contents($path);
+            $response->headers->set('Content-Type',self::mimeTypes[$extension]);
+            $response->setContent($content);
+        } else {
+            $response->setContent("No file was found".$path);
+            $response->setStatusCode(403);
+        }
+
+        return $response;
+    }
+
+}
+
+
+?>
