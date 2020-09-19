193aa952abc90869336b0d6aae8d45988621fac1
Alexander Zenchenko <zenchenko.alexander@gmail.com>
2020-09-19 17:47:04 +0300

    home page with tags cloud

---
+++ public/client/js/tagsCloud.js
@@ -0,0 +1,34 @@
+    // Themes begin
+    am4core.useTheme(am4themes_animated);
+    // Themes end
+
+    var chart = am4core.create("chartdiv", am4plugins_wordCloud.WordCloud);
+    chart.fontFamily = "Courier New";
+    var series = chart.series.push(new am4plugins_wordCloud.WordCloudSeries());
+    series.randomness = 0.1;
+    series.rotationThreshold = 0.5;
+
+    series.dataFields.word = "tag";
+    series.dataFields.value = "count";
+
+    series.heatRules.push({
+        "target": series.labels.template,
+        "property": "fill",
+        "min": am4core.color("#0000CC"),
+        "max": am4core.color("#CC00CC"),
+        "dataField": "value"
+    });
+
+    series.labels.template.urlTarget = "_blank";
+    series.labels.template.tooltipText = "{word}: {value}";
+
+    var hoverState = series.labels.template.states.create("hover");
+    hoverState.properties.fill = am4core.color("#FF0000");
+
+    var subtitle = chart.titles.create();
+    subtitle.text = "(click to open)";
+
+    var title = chart.titles.create();
+    title.text = "Most Popular Tags @ ODClient";
+    title.fontSize = 20;
+    title.fontWeight = "800";
