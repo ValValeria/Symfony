694f192b61be676a571557a8ded882359ac6eb5c
vadym <12vadym098696@gmail.com>
2020-09-27 11:05:33 +0300

    home page form

--- src/Controller/MainPage.php
+++ src/Controller/MainPage.php
@@ -2,15 +2,15 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController as AbsController;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
-class MainPage extends AbsController{
-    
+class MainPage extends AbstractController
+{
     public function mainPage()
     {
-        return $this->render('Pages/home_page.html.twig');
+        return $this->render('Pages/home_page.html.twig', array(
+        ));
     }
 }
-
-
-?>
