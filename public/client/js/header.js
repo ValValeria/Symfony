29b2e80ee86251df70d88c639f3ed65fcb5346bc
ValValeria <led2017l@gmail.com>
2020-09-29 16:30:07 +0300

    small fixes

--- public/client/js/header.js
+++ public/client/js/header.js
@@ -1,10 +1,8 @@
-document.addEventListener('DOMContentLoaded',()=>{
-
-    const path = window.location.pathname;
+const path = window.location.pathname;
     
-    const links = Array.from(document.querySelectorAll('nav .nav-link'))
+const links = Array.from(document.querySelectorAll('nav .nav-link'))
 
-    links.forEach(el=>{
+links.forEach(el=>{
         const elPath = el.getAttribute('href');
         
         if(elPath==path){
@@ -14,10 +12,33 @@ document.addEventListener('DOMContentLoaded',()=>{
         }
     })
 
-    const menuIcon = document.querySelector('.navbar-toggler');
+const menuIcon = document.querySelector('.navbar-toggler');
 
-    menuIcon.onclick = ()=>{
+menuIcon.onclick = ()=>{
         document.querySelector('#navbarSupportedContent').classList.toggle('collapse')
-    }
+}
+
+otherSetting();
           
-})
+function otherSetting(){
+    const hideEl = document.querySelector('#tags_cloud');
+
+    if(hideEl){
+        hideEl.classList.add('none');
+
+        document.querySelector('#btn').onclick = ()=>{
+          hideEl.classList.toggle('none');
+          hideEl.classList.toggle('animation');     
+        }
+    }
+
+    window.onload = ()=>{
+        const loadingElem = document.querySelector('#loading');
+        loadingElem.classList.add('none');
+    }
+}
+
+
+
+
+
